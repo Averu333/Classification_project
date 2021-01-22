89de2aa296a1fcedef95f5540053da6220f6a66b
Mika Kosonen <kosonem4@aalto.fi>
2021-01-22 15:13:58 +0200

    Fix evaluation bugs
    Tested version of evaluation.

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -2,6 +2,7 @@ import torch
 import torchvision
 from sklearn.metrics import confusion_matrix
 import numpy as np
+from torchvision import transforms
 
 def evaluate_model(model, data_loader_test, device):
     '''
@@ -18,45 +19,23 @@ def evaluate_model(model, data_loader_test, device):
     '''
     model.eval()
     model.to(device=device)
-    data_log = np.zeros((10,10))
+    data_log = np.zeros((10,10), dtype=np.int32)
+    matrix_labels = [i for i in range(10)]
     for batch_num, data in enumerate(data_loader_test):
         # Usualy data formating is done in custom dataset object
         # However this time I'm using the dataset from torchvision
         # so I have to change the data format here
         images = torch.stack([transforms.ToTensor()(i[0]).squeeze(0) for i in data])
         images = images.to(device=device)
-        targets = torch.stack([torch.tensor(i[1]) for i in data])
-        targets = targets.to(device=device)
+        targets = np.array([i[1] for i in data])
         
         with torch.no_grad():
             prediction = model(images)
-        #Choose max prediction to represent class and compare to target    
-        data_log += confusion_matrix(prediction, targets)
+        #Choose max prediction to represent class and compare to target
+        prediction = prediction.detach().cpu().numpy()
+        prediction = np.argmax(prediction, axis=1)  
+        conf = confusion_matrix(prediction, targets, labels=matrix_labels)  
+        data_log += conf
     
     score = np.sum(np.diagonal(data_log))/np.sum(data_log)
-    # return score, data_log    
-    return 0, 0
-
-
-def mycollate(batch):
-    return batch
-
-if __name__ == "__main__":
-    device = 'cpu'
-    model_path = '/workspaces/cvproject/weights/1_'
-    
-    model = get_classifier_model('resnet', num_classes=10, use_pretrained=True)
-    model.to(device)
-    model.load_state_dict(torch.load(PATH))
-    
-    dataset_test = torchvision.datasets.CIFAR10(root='/workspaces/cvproject/data/test',
-                                                train=False,
-                                                download=False)
-    
-    data_loader_test = torch.utils.data.DataLoader(dataset_test,
-                                                   batch_size=10,
-                                                   shuffle=False,
-                                                   num_workers=0,
-                                                   collate_fn=mycollate)
-    
-    evaluate_model(model, data_loader_test, device)
+    return score, data_log
