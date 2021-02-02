d0520ac4cb6a8d064e606e441e86b4efefc9277e
Mika Kosonen <kosonem4@aalto.fi>
2021-02-02 18:19:18 +0200

    Change save method
    Changed save_weights to work for full model training

--- utils/myutils.py
+++ utils/myutils.py
@@ -2,42 +2,20 @@ import os
 import torch
 import wandb
 
-def save_weights(weights_folder, model_weights, eval_score, model_name, use_wandb=False):
+def save_weights(weights_folder, model_weights, model_name):
     '''
     An helper function that saves model weights if its evaluation score is higher than
     the best models in weights_folder. The save name is score_wandb.run.name.
     Args:
         weights_folder (str): function looks this folder for best model and replaces the best model if needed.
         model_weights (dict): model weights that will be saved if score is better than current best.
-        eval_score (float): the score for model.
-        use_wandb (bool): A bool to determine if wandb is in use. If in use the save name will be score_wandb.run.name
-                            if false the save name will be score_.
+        model_name (str): model_name
     Returns:
         Nothing
     '''
-    #Create filename and savepath if new weights are saved
-    if use_wandb:
-        model_save_name = '{}_{}_{}'.format(str(eval_score), model_name[0], wandb.run.name)
-    else:
-        model_save_name = '{}_{}_'.format(str(eval_score), model_name[0])
+    model_save_name = model_name + '_full_training'
     model_save_path = os.path.join(weights_folder, model_save_name)
-    
-    #List all current networks and check if current score is better
-    #The networks score is saved to the name
-    no_saved_networks = True
-    network_names = os.listdir(weights_folder)
-    for name in network_names:
-        name_num_part = name.split('_')[0]
-        if string_is_num(name_num_part):
-            name_num_part = float(name_num_part)
-            no_saved_networks = False
-            if name_num_part < eval_score:
-                os.remove(os.path.join(weights_folder, name))
-                torch.save(model_weights, model_save_path)
-    
-    #If no weights in folder save this as first
-    if no_saved_networks:
-        torch.save(model_weights, model_save_path)
+    torch.save(model_weights, model_save_path)
         
 def mycollate(batch):
     return batch
